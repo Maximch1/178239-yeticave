be11cb3542d0fe2085a3c3728e723c460353d772
Maximch1 <maximch@bk.ru>
2019-02-10 20:15:15 +0300

    9.Создание схемы БД личного проекта

--- schema.sql
+++ schema.sql
@@ -6,42 +6,45 @@ USE yeticave;
 
 CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title CHAR(32) NOT NULL UNIQUE
+  title VARCHAR(20) NOT NULL UNIQUE
 );
+
+CREATE UNIQUE INDEX category_id ON category(id);
 
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  email CHAR(128) NOT NULL UNIQUE,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email VARCHAR(255) NOT NULL UNIQUE,
   name INT NOT NULL UNIQUE,
   password VARCHAR(32) NOT NULL,
   contacts TEXT NOT NULL,
   avatar VARCHAR(255)
 );
 
+CREATE UNIQUE INDEX users_id ON users(id);
+
 CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title INT NOT NULL UNIQUE,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  title VARCHAR(128) NOT NULL UNIQUE,
   description TEXT NOT NULL,
   image VARCHAR(255) NOT NULL,
   category_id INT NOT NULL,
-  price CHAR(7) NOT NULL,
-  end_date DATETIME NOT NULL,
+  price INT NOT NULL,
+  end_time TIMESTAMP DEFAULT NULL,
   step_rate INT NOT NULL,
   users_id INT NOT NULL,
   winner_id INT
 );
 
+CREATE UNIQUE INDEX lot_id ON lot(id);
+
 CREATE TABLE bet (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   rate CHAR(7) NOT NULL,
   users_id INT NOT NULL,
   lot_id INT NOT NULL
 );
 
-CREATE UNIQUE INDEX category_id ON category(id);
-CREATE UNIQUE INDEX users_id ON users(id);
-CREATE UNIQUE INDEX lot_id ON lot(id);
 CREATE UNIQUE INDEX bet_id ON bet(id);
-
-CREATE INDEX winner_id ON users(id);
