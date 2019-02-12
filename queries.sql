d467ca1877c7ee567049a74c4619182cb9601b79
1 <1>
2019-02-12 15:22:56 +0300

    10.Пишем SQL-запросы

--- queries.sql
+++ queries.sql
@@ -27,34 +27,34 @@ INSERT INTO bets (rate, user_id, lot_id) VALUE
 
 
 -- получаем все категрии
-SELECT title FROM categories;
+SELECT * FROM categories;
 
 -- получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории;
--- 1 - вариант. Более проработанный с отображением выставленной последней ставкой.
 SELECT l.title Наименование_лота, l.price Начальная_цена, l.image Изображение, MAX(b.rate) Цена, c.title Категория
-FROM lots l, categories c, bets b
-WHERE c.id = l.category_id AND l.id = b.lot_id AND l.winner_id IS NULL GROUP BY l.id ORDER BY l.id DESC;
--- 2 - вариант
-SELECT l.title, l.price, l.image, c.title
-FROM lots l JOIN categories c ON l.category_id = c.id
-WHERE l.end_time >= NOW() ORDER BY l.create_time DESC;
-
+FROM lots l
+  JOIN categories c ON c.id = l.category_id
+  JOIN bets b ON l.id = b.lot_id
+WHERE l.winner_id IS NULL GROUP BY l.id ORDER BY l.id DESC;
 
 -- показать лот по его id. Получите также название категории, к которой принадлежит лот
 SELECT l.id, c.title
-FROM lots l, categories c
-WHERE c.id = l.category_id AND l.id = '1';
+FROM lots l
+  JOIN categories c ON c.id = l.category_id
+WHERE l.id = '1';
 
 --  обновить название лота по его идентификатору;
-UPDATE lots SET title = '' WHERE id = '1';
+UPDATE lots SET title = 'Super Snowboard' WHERE id = '1';
 
 -- получить список самых свежих ставок для лота по его идентификатору;
-SELECT l.*, l.title, l.image, c.title, l.price, b.rate, u.name
-FROM lots l, bets b, categories c, users u
-WHERE b.lot_id = l.id AND l.category_id = c.id AND l.user_id = u.id AND l.id = 3 ORDER BY b.id DESC;
+SELECT l.title, l.image, c.title, l.price, b.rate, u.name
+FROM lots l
+  JOIN categories c ON c.id = l.category_id
+  JOIN bets b ON l.id = b.lot_id
+  JOIN users u ON l.user_id = u.id
+WHERE l.id = 3 ORDER BY b.id DESC;
 
 
 -- получить список самых свежих ставок для лота по его идентификатору;
 SELECT *
-FROM bets b
-WHERE b.lot_id=1 ORDER BY b.create_time DESC;
+FROM bets
+WHERE lot_id = 1 ORDER BY create_time DESC;
